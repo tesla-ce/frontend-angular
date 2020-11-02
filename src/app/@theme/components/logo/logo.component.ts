95ed8d895a44763256892a6b9770e910203d5b4c
devstepbcn <devstep.bcn@gmail.com>
2020-11-02 13:08:44 +0100

    Logo + text

--- src/app/@theme/components/logo/logo.component.ts
+++ src/app/@theme/components/logo/logo.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LogoComponent implements OnInit {
 
-  @Input() type: string = "image"; // ["image", "text"]
+  @Input() type: string = "image+text"; // ["image", "text", "image+text"]
   @Input() title: string = "TeSLA-CE";
   @Input() logo: string = "/assets/logo.png";
 
