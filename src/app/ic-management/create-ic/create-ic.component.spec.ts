ecbf2c552a123417460e16e8428421d46c80d698
devstepbcn <devstep.bcn@gmail.com>
2020-11-17 16:54:13 +0100

    remove not implemented test specs

--- src/app/ic-management/create-ic/create-ic.component.spec.ts
+++ src/app/ic-management/create-ic/create-ic.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateIcComponent } from './create-ic.component';
 
 describe('CreateIcComponent', () => {
-  let component: CreateIcComponent;
-  let fixture: ComponentFixture<CreateIcComponent>;
+  // let component: CreateIcComponent;
+  // let fixture: ComponentFixture<CreateIcComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreateIcComponent ],
-    })
-    .compileComponents();
-  }));
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ CreateIcComponent ],
+  //   })
+  //   .compileComponents();
+  // }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreateIcComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(CreateIcComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
