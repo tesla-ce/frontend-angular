312b45f34a11c1b0d5e8e4d6033e6950e1fb26b8
devstepbcn <devstep.bcn@gmail.com>
2021-03-12 12:52:11 +0100

    launcher component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,13 @@ import {
 import {AuthGuardAuthenticated} from './@core/auth/guards/auth-guard-authenticated';
 // import { AuthGuardAdmin } from './@core/auth/guards/auth-guard-admin';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LauncherComponent } from './@core/launcher/launcher.component';
 
 export const routes: Routes = [
+  {
+    path: 'auth/launcher',
+    component: LauncherComponent,
+  },
   {
     path: 'dashboard',
     component: DashboardComponent,
