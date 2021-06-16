e0ade751b74b07a1eda8969617cd04ad0afe3815
devstepbcn <devstep.bcn@gmail.com>
2021-06-16 10:03:04 +0200

    get instruments from institution

--- src/app/@core/data/api-course.service.ts
+++ src/app/@core/data/api-course.service.ts
@@ -122,7 +122,7 @@ export class ApiCourseService {
   // API: GET /instrument/
   public getAllInstruments(): Observable<any> {
     return this.http
-      .get(this.apiUrl + '/admin/instrument/')
+      .get(this.endpointUrl + '/instrument/')
       .pipe(
         map((response: any) => {
           if (response?.results) return response.results;
