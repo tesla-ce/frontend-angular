90bbc85a328af39c432c6bad11852fa127d6deb2
Xavier Baró <xavierbaro@gmail.com>
2021-09-08 16:00:52 +0200

    Added results to FR audit

--- src/app/pages/my-courses/course-activity/course-report/course-report-audit/course-report-audit.component.ts
+++ src/app/pages/my-courses/course-activity/course-report/course-report-audit/course-report-audit.component.ts
@@ -4,8 +4,8 @@ import { AuthService } from '../../../../../@core/auth/auth.service';
 import { TranslateService } from '@ngx-translate/core';
 import { formatDate, Location } from '@angular/common';
 import { ApiCourseService } from '../../../../../@core/data/api-course.service';
-import { ListCellSumaryComponent } from '../course-report-list/list-cell-sumary.component';
 import { InstitutionUser } from '../../../../../@core/models/user';
+import { ReportAudit } from '../../../../../@core/models/report';
 import { ApiReportService } from '../../../../../@core/data/api-report.service';
 import { HttpClient } from '@angular/common/http';
 import { MATERIAL_TESLA_THEME } from '../../../../../@theme/styles/material/theme.material-tesla';
@@ -19,7 +19,7 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 
 export class CourseReportAuditComponent implements OnInit {
-  customOptions: OwlOptions = {
+  customOptionsEnrolment: OwlOptions = {
     loop: true,
     mouseDrag: false,
     touchDrag: false,
@@ -43,6 +43,54 @@ export class CourseReportAuditComponent implements OnInit {
     },
     nav: true,
   };
+  customOptionsResults: OwlOptions = {
+    loop: false,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 3,
+      },
+    },
+    nav: true,
+  };
+  customOptionsFaces: OwlOptions = {
+    loop: false,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      740: {
+        items: 1,
+      },
+      940: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
   courseId: number;
   activityId: number;
   reportId: number;
@@ -50,7 +98,8 @@ export class CourseReportAuditComponent implements OnInit {
   endpoint: string;
   loading: boolean = true;
   audit: any = null;
-  slidesStore: any[];
+  enrolmentSlidesStore: any[];
+  resultsSlidesStore: any[];
 
   constructor(
     private authService: AuthService,
@@ -76,19 +125,8 @@ export class CourseReportAuditComponent implements OnInit {
         this.apiReportService.getActivityReportAudit(
           user.institution.id, this.courseId, this.activityId, this.reportId, this.instrumentId).subscribe(audit => {
           this.audit = audit;
-          this.slidesStore = [];
-          if (this.audit !== null && this.audit.audit !== null && this.audit.audit.hasOwnProperty('enrolment_samples')) {
-            const sample_ids = Object.keys(this.audit.audit.enrolment_samples);
-            for (const sid of sample_ids) {
-              const sample = this.audit.audit.enrolment_samples[sid];
-              this.slidesStore.push({
-                src: sample.data,
-                alt: 'Sample ' + sample.id,
-                title: 'Enrolment Sample ' + sample.id,
-                id: sid,
-              });
-            }
-          }
+          this.load_enrolment(audit);
+          this.load_results(audit);
           this.loading = false;
         });
       }
@@ -96,6 +134,51 @@ export class CourseReportAuditComponent implements OnInit {
   }
 
   back() { this.location.back(); }
+
+  load_enrolment(data: ReportAudit): void {
+    this.enrolmentSlidesStore = [];
+    if (data !== null && data.audit !== null && data.audit.hasOwnProperty('enrolment_samples')) {
+      const sample_ids = Object.keys(data.audit.enrolment_samples);
+      for (const sid of sample_ids) {
+        const sample = data.audit.enrolment_samples[sid];
+        this.enrolmentSlidesStore.push({
+          src: sample.data,
+          alt: 'Sample ' + sid,
+          title: 'Enrolment Sample ' + sid,
+          id: sid,
+        });
+      }
+    }
+  }
+
+  load_results(data: ReportAudit): void {
+    this.resultsSlidesStore = [];
+    if (data !== null && data.audit !== null) {
+      const requests_ids = Object.keys(data.audit.requests);
+      for (const rid of requests_ids) {
+        const request = data.audit.requests[rid];
+        const request_results: any[] = [];
+        for (const pid of Object.keys(request.results)) {
+          request_results.push({
+            acronym: data.audit.providers[pid].acronym,
+            result: request.results[pid].result,
+            enrolment: data.audit.providers[pid].enrolment,
+            faces: request.results[pid].audit.faces,
+            status: request.results[pid].status,
+          });
+        }
+        this.resultsSlidesStore.push({
+          src: request.data,
+          alt: 'Request ' + rid,
+          title: 'Request ' + rid,
+          id: rid,
+          created_at: formatDate(request.created_at, 'dd/MM/yyyy h:mm:ss', 'en-US'),
+          result: request.result,
+          providers: request_results,
+        });
+      }
+    }
+  }
 }
 
 
