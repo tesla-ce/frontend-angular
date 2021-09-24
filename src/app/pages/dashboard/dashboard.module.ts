4e0ee8255fd2d1d989f7bd81f25ee08623b78a9f
Xavier Baró <xavierbaro@gmail.com>
2021-09-24 20:41:20 +0200

    Added calendar widget

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -5,7 +5,7 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SideMenuModule } from '../../side-menu/side-menu.module';
 import { DashboardDefaultComponent } from './dashboard-default/dashboard-default.component';
-import { NbButtonModule, NbCardModule, NbListModule, NbIconModule } from '@nebular/theme';
+import {NbButtonModule, NbCardModule, NbListModule, NbIconModule, NbCalendarModule} from '@nebular/theme';
 import { SharedModule } from '../../shared/shared.module';
 import { JsonFormsModule } from '@jsonforms/angular';
 import { JsonFormsAngularMaterialModule } from '@jsonforms/angular-material';
@@ -13,6 +13,8 @@ import { GridsterModule } from 'angular-gridster2';
 import {DashboardWidgetsComponent} from './dashboard-widgets/dashboard-widgets.component';
 import {BaseWidgetComponent} from './dashboard-widgets/widgets/base-widget.component';
 import {CoursesWidgetComponent} from './dashboard-widgets/widgets/courses/courses-widget.component';
+import {CalendarWidgetComponent} from './dashboard-widgets/widgets/calendar/calendar-widget.component';
+import {DayCellComponent} from './dashboard-widgets/widgets/calendar/day-cell/day-cell.component';
 
 @NgModule({
   imports: [
@@ -31,6 +33,7 @@ import {CoursesWidgetComponent} from './dashboard-widgets/widgets/courses/course
     JsonFormsAngularMaterialModule,
     // Gridster 2 module
     GridsterModule,
+    NbCalendarModule,
   ],
   declarations: [
     DashboardComponent,
@@ -38,6 +41,8 @@ import {CoursesWidgetComponent} from './dashboard-widgets/widgets/courses/course
     DashboardWidgetsComponent,
     BaseWidgetComponent,
     CoursesWidgetComponent,
+    CalendarWidgetComponent,
+    DayCellComponent,
   ],
 })
 export class DashboardModule {
